52d2839e8147a04d3cecdef609b33b961e2af29e
bobtheassailant <louie@Louies-MacBook.local>
2017-01-08 03:19:42 -0700

    Project Files
    Initial project upload.

---
+++ weather.js
@@ -0,0 +1,5 @@
+$('#submit_button').click(function() {
+  var apiKey = 'a15495a058055c781ce85098cfc5b7b6';
+  var baseHTTP = 'https://api.darksky.net/forecast/' + apiKey;
+  console.log(baseHTTP);
+});
